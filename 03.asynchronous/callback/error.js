099545b7b47d26578a1997706165d87bb212168e
horishota6566 <shou6130@gmail.com>
2025-12-02 12:14:51 +0900

    fix(callback): INSERT エラー時も count 更新と後続処理を継続するように修正

--- 03.asynchronous/callback/error.js
+++ 03.asynchronous/callback/error.js
@@ -10,15 +10,8 @@ const db = new sqlite3.Database(":memory:", () => {
       let count = titles.length;
       titles.forEach((title) => {
         db.run("INSERT INTO books (title) VALUES (?)", [title], function (err) {
-          if (err) {
-            console.error(err.message);
-            db.run("DROP TABLE books", () => {
-              db.close();
-            });
-            return;
-          }
+          err ? console.error(err.message) : console.log(this.lastID);
 
-          console.log(this.lastID);
           count--;
 
           if (count === 0) {
