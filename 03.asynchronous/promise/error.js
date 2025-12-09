aebda311025ea38cb2e9dfa6f244a180d64f6f65
horishota6566 <shou6130@gmail.com>
2025-12-09 10:52:41 +0900

    refactor(promise): エラー時の処理をthenの第二引数からcatchで渡すように変更

--- 03.asynchronous/promise/error.js
+++ 03.asynchronous/promise/error.js
@@ -16,26 +16,22 @@ openDB()
   .then(() => {
     return run(db, "INSERT INTO books (title) VALUES (?)", [null]);
   })
-  .then(
-    (result) => {
-      console.log(result.lastID);
+  .then((result) => {
+    console.log(result.lastID);
 
-      return get(db, "SELECT id, title, content FROM books");
-    },
-    (err) => {
-      console.error(err.message);
+    return get(db, "SELECT id, title, content FROM books");
+  })
+  .catch((err) => {
+    console.error(err.message);
 
-      return get(db, "SELECT id, title, content FROM books");
-    },
-  )
-  .then(
-    (row) => {
-      console.log(row);
+    return get(db, "SELECT id, title, content FROM books");
+  })
+  .then((row) => {
+    console.log(row);
 
-      return run(db, "DROP TABLE books");
-    },
-    (err) => {
-      console.error(err.message);
-    },
-  )
+    return run(db, "DROP TABLE books");
+  })
+  .catch((err) => {
+    console.error(err.message);
+  })
   .then(() => close(db));
