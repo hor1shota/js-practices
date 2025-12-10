1aa8c1d8f08b628a6462d3f15c7da476d0bdbb91
horishota6566 <shou6130@gmail.com>
2025-12-10 09:12:28 +0900

    fix(async): errがエラーオブジェクトであることを確認するように修正

--- 03.asynchronous/async/error.js
+++ 03.asynchronous/async/error.js
@@ -14,7 +14,7 @@ try {
 
   console.log(result.lastID);
 } catch (err) {
-  if (err?.code === "SQLITE_CONSTRAINT") {
+  if (err instanceof Error && err.code === "SQLITE_CONSTRAINT") {
     console.error(err.message);
   } else {
     throw err;
@@ -26,7 +26,7 @@ try {
 
   console.log(row);
 } catch (err) {
-  if (err?.code === "SQLITE_ERROR") {
+  if (err instanceof Error && err.code === "SQLITE_ERROR") {
     console.error(err.message);
   } else {
     throw err;
